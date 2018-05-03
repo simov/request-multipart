0696a60f3fadcca71e8711d332a1876cf56a9221
simov <simeonvelichkov@gmail.com>
2018-05-03 17:50:38 +0300

    Do not set transfer-encoding in multipart middleware

--- mw.js
+++ mw.js
@@ -4,13 +4,6 @@ var lib = require('./lib/multipart')
 
 module.exports = (multipart) => ({options, options: {headers}}) => {
 
-  var encoding = Object.keys(headers)
-    .find((name) => name.toLowerCase() === 'transfer-encoding')
-
-  if (!encoding) {
-    headers['transfer-encoding'] = 'chunked'
-  }
-
   var content = Object.keys(headers)
     .find((name) => name.toLowerCase() === 'content-type')
 
